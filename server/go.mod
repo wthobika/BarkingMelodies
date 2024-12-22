70523b5595c7105bfed86a110b4dac6f70cf7818
wthobika <wthobika@buffalo.edu>
2024-12-22 13:31:30 -0500

    Added a mod file to server.

---
+++ server/go.mod
@@ -0,0 +1,10 @@
+module github.com/wthobika/BarkingMelodies/server
+
+go 1.23.4
+
+require (
+	github.com/go-chi/chi/v5 v5.2.0
+	github.com/wthobika/BarkingMelodies/src v0.0.0-00010101000000-000000000000
+)
+
+replace github.com/wthobika/BarkingMelodies/src => ../src
--- server/main.go
+++ server/main.go
@@ -4,10 +4,10 @@ import(
 	"net/http"
 	"github.com/go-chi/chi/v5"
 
-	"github.com/wthobika/BarkingMelodies/src"
+	"github.com/wthobika/BarkingMelodies/src"     //I have no idea why this import wont work but it wont so whatever. Maybe I'll fix it in the future.
 )
 
-
+//Where the server starts
 func main(){
 	router := chi.NewRouter()
 
@@ -18,6 +18,11 @@ func main(){
 	http.ListenAndServe(":8080", router)
 }
 
+
+
+//I couldn't figure out how to import files so I will write the logic/backend here temporarily
+
+//Serves basic index.html file
 // func indexHandler(w http.ResponseWriter, r *http.Request){
 // 	http.ServeFile(w, r, "../static/html/index.html")
 // }
